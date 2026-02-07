6f32280c621ab77dd4f9cdc68d2000e8c4d39232
--global <--global>
2026-02-07 11:06:37 +0000

    fix: echoのバージョンを戻す

--- backend/internal/handler/middleware/jwt.go
+++ backend/internal/handler/middleware/jwt.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v4"
 	"github.com/gigaonion/taskalyst/backend/internal/config"
 	"github.com/gigaonion/taskalyst/backend/pkg/auth"
 )
